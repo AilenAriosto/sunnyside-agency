eeab69af27591a0216ff543ae7d22dd142fec8bd
Ailen Ariosto <ailen-1505@hotmail.com>
2022-01-12 20:12:00 -0300

    add jquery

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,27 +2,24 @@ $(document).ready(function(){
 
     let buttonBurguer = $('#buttonBurguer')
     let menu = $('#menu')
-    let elementsList = $('.header_nav_listContainer--elements')
-    let elementsListContainer = $('.elementsListContainer')
 
     buttonBurguer.click(function(){
-        menu.css("display", "block")
-        menu.css("background", "white")
-            .css("textAlign", "center")
-            .css("position", "absolute")
-
-        buttonBurguer.css("position", "relative")
-
-        elementsList.css("color", "#818498")
-
-        elementsListContainer.css("display", "flex")
-                            .css("flexDirection", "column")
-                            .css("height", "22rem")
-                            .css("padding", "40px 90px")
-                            .css("justifyContent", "space-evenly")
-
+        menu.toggle("scale", 500)
     })
 
+
+
+    // ------------------Slider
+
+
+    // Create slideshow instances
+    var $slideshow = $('.slideshow').slides(),
+
+    // Access an instance API
+    api = $slideshow.data('slides');
+
+
+   
 
 
 })
