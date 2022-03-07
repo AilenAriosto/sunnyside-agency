e27ea1be9de0cda4e6df3340f538687dd8f18de4
Ailen Ariosto <ailen-1505@hotmail.com>
2022-03-07 12:33:11 -0300

    add jquery testimonials

--- dist/assets/js/scripts.js
+++ dist/assets/js/scripts.js
@@ -1,25 +1,70 @@
 $(document).ready(function(){
+    
+    
+    // Burguer
+    
+    const showBurguer = () =>{
+        let buttonBurguer = $('#buttonBurguer')
+        let menu = $('#menu')
 
-    let buttonBurguer = $('#buttonBurguer')
-    let menu = $('#menu')
+        buttonBurguer.click(function(){
+            menu.toggle("scale", 500)
+        })
+    }
 
-    buttonBurguer.click(function(){
-        menu.toggle("scale", 500)
-    })
+    // Slider
+    
+    const slider = () =>{
+        var $slideshow = $('.slideshow').slides(),
+        api = $slideshow.data('slides');
+    }
+    
+    // Array Testimonials
+    
+    const testimonials = () =>{
+        let testimonialData = [
+            {
+                image: '../images/image-emily.jpg',
+                comment: 'We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.',
+                name: 'Emily R.',
+                profession: 'Marketing Director'
+            },
+            {
+                image: '../images/image-thomas.jpg',
+                comment: 'Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.',
+                name: 'Thomas S.',
+                profession: 'Chief Operating Officer'
+            },
+            {
+                image: '../images/image-jennie.jpg',
+                comment: `Incredible end result! Our sales increased over 400% when we worked with Sunnyside.<br>Highly recommended!`,
+                name: 'Jennie F.',
+                profession: 'Business Owner'
+            }
+        ]
+        testimonialData.forEach((i, e) =>{
+            
+            let testimonialPost = `
+                <article class="testimonials--card">
+                    <figure class="testimonials_card--image"></figure>
+                    <p class="testimonials_card--about">${i.comment}</p>
+                    <h5 class="testimonials_card--name">${i.name}</h5>
+                    <p class="testimonials_card--profesion">${i.profession}</p>
+                </article>
+            `
+            
+            $("#postTestimonials").append(testimonialPost)
+        })   
+        
+    }
+    
 
 
-
-    // ------------------Slider
-
-
-    // Create slideshow instances
-    var $slideshow = $('.slideshow').slides(),
-
-    // Access an instance API
-    api = $slideshow.data('slides');
-
-
-   
+   // Call function
+    
+    showBurguer()
+    slider()
+    testimonials()
 
 
 })
--- readme.md
+++ readme.md
@@ -1,16 +1,41 @@
-// Plantilla
+### Links
 
-https://gist.github.com/Villanuevand/6386899f70346d4580c723232524d35a
+- Solution URL: https://sunnyside-agency-jquery.netlify.app
+- Live Site URL: https://github.com/AilenAriosto/sunnyside-agency
+
+## My process
+
+### Built with
+
+- HTML5 Semántico
+- CSS3
+- Flexbox
+- CSS Grid
+- Responsive Design
+- Pug
+- Scss
+- Accesibilidad 
+- jQuery
+- Git
+- Gulp
 
 
+### What I learned
 
-- HTML Semantico 
-- Pug - Sass
-- CSS limpio (Agregar badge)
+- jQuery
 
-- Accesibilidad
-- SEO
-- CSS optimizado
-- Arreglar bug en jQuery
-- Agregar mas jQuery
-- Organizar readme
+
+## Author
+
+- Website - Ailen Ariosto (https://ailenariostoportafolio.netlify.app)
+- Frontend Mentor - @AilenAriosto (https://www.frontendmentor.io/profile/AilenAriosto)
+- Github - AilenAriosto (https://github.com/AilenAriosto)
+- Linkedin - Ailen Ariosto (https://www.linkedin.com/in/ailen-ariosto)
+
+**Note: Delete this note and add/remove/edit lines above based on what links you'd like to share.**
+
+## Acknowledgments
+
+This is where you can give a hat tip to anyone who helped you out on this project. Perhaps you worked in a team or got some inspiration from someone else's solution. This is the perfect place to give them some credit.
+
+**Note: Delete this note and edit this section's content as necessary. If you completed this challenge by yourself, feel free to delete this section entirely.**
